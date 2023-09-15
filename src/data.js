f4e04487c867cb667b6d7eafec521ebb4ba597a3
Eliana-2 <eliana.guizar1@gmail.com>
2023-09-15 16:44:33 -0700

    Setup project and add initial general info

---
+++ src/data.js
@@ -0,0 +1,8 @@
+const initialGeneralInformation = {
+  name: 'Firstname Lastname',
+  email: 'email@email.com',
+  phoneNumber: '999-9999',
+  location: 'Someplace, Earth'
+}
+
+export {initialGeneralInformation}
